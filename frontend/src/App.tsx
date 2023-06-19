0894aed0f75941a00a66a44cfd4b8cb01a48543c
Abhishek <abhishekgupta954@gmail.com>
2023-06-19 23:49:36 +0530

    some ui fixes user rating increased to 200

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -42,7 +42,7 @@ function App() {
 				rank: userInfo.rank,
 			});
 
-			const startR = Math.floor(userInfo.rating / 100) * 100;
+			const startR = Math.floor(userInfo.rating / 100) * 100 + 200;
 
 
 			setLadderData({
@@ -82,12 +82,9 @@ function App() {
 	}, [userData]);
 
 	return (
-		<div className='w-full container bg-slate-700'>
+		<div className='w-full bg-slate-700'>
 			<Header heading='ACD Ladders!' />
 			<div className='w-full p-2'>
-
-
-
 
 				<div className='flex mt-4'>
 					<LadderSelector startRating={900} endRating={3600} step={100} setLadderData={setLadderData} ladderData={ladderData} selected={selected} setSelected={setSelected} />
@@ -104,23 +101,25 @@ function App() {
 						<button className='btn btn-primary' onClick={loadUser}><i className="bi bi-search"></i> Search</button>
 					</div>
 				</div> */}
-					{
+
+					{/* user iamge removed for while */}
+					{/* {
 						userData &&
 						<div className="pr-2">
-							<img className='w-10 h-10 rounded-full' src={userData.image} alt={userData.handle} />
+							<img className='min-h-10 min-w-10 rounded-full' src={userData.image} alt={userData.handle} />
 						</div>
-					}
+					} */}
 
 
 
-					<div className="relative mx-auto w-96">
+					<div className="relative mx-auto w-auto">
 						<input className="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5  font-normal text-gray-100 bg-slate-900 rounded-full transition ease-in-outfocus:text-gray-100 " placeholder="CF Handle"
 							value={user} onChange={(e) => setUser(e.target.value)} onKeyDown={(e) => {
 								if (e.key === 'Enter')
 									loadUser();
 							}}
 						/>
-						<button type="submit" className="absolute right-0 top-0 mt-2.5 mr-4" onClick={loadUser}>
+						{/* <button type="submit" className="absolute right-0 top-0 mt-2.5 mr-4" onClick={loadUser}>
 							<svg className="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
 								version="1.1" id="Capa_1" x="0px" y="0px"
 								viewBox="0 0 56.966 56.966"
@@ -128,7 +127,7 @@ function App() {
 								<path
 									d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
 							</svg>
-						</button>
+						</button> */}
 					</div>
 
 				</div>
