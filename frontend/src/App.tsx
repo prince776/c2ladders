9fe8fd5e6b923c190cabcabefed3b803593d7750
Abhishek <abhishekgupta954@gmail.com>
2023-06-24 12:07:56 +0530

    Ui Fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -42,6 +42,7 @@ function App() {
 
   const [tag, setTag] = useState(false)
 
+
   const loadUser = async () => {
     try {
       const userDataRes = await httpClient.request({
@@ -101,14 +102,18 @@ function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [userData]);
 
+  
+
+
+
+
   return (
     <div className="w-full bg-color">
       <Header heading="ACD Ladders!" />
       <div className="w-full p-3 md:p-10">
-        <div className="top-row flex flex-col-reverse justify-between  md:flex-row sticky top-0 z-10 bg-[#2b2c3e] md:m-10">
-
+        <div className="w-full top-row flex flex-col-reverse justify-between md:flex-row sticky top-0 z-10 bg-[#2b2c3e] pt-2">
           <LadderSelector
-            showRating={11}
+            showRating={10}
             startRating={900}
             endRating={3600}
             step={100}
@@ -117,8 +122,9 @@ function App() {
             selected={selected}
             setSelected={setSelected}
           />
-          <div className="flex flex-row w-full justify-between py-2 md:px-10">
-            <div className="search-bar relative w-auto justify-between">
+
+          <div className="w-full md:w-3/5 flex flex-row justify-between py-2 md:px-10 md:justify-center">
+            <div className="search-bar relative w-auto justify-between mx-2">
               <input
                 className="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5  font-normal text-gray-100 bg-slate-900 rounded-full transition ease-in-outfocus:text-gray-100 "
                 placeholder="CF Handle"
@@ -146,10 +152,10 @@ function App() {
                   />
                 </svg>
               </button>
-              d</div>
+            </div>
 
 
-            <div className="">
+            <div className="ml-2 pt-1.5">
               <input
                 className="mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]"
                 type="checkbox"
@@ -158,14 +164,12 @@ function App() {
                 onChange={(e) => setTag(e.target.checked)}
               />
               <label
-                className="inline pl-[0.15rem] hover:cursor-pointer text-gray-200"
-              >Tags
+                className="inline pl-[0.15rem] hover:cursor-pointer text-gray-200 text-lg mr-1"
+              >{tag ? "Freq" : "Tags"}
               </label>
             </div>
           </div>
         </div>
-
-
 
         <UserCard userData={userData} userStats={userStats} />
 
@@ -176,7 +180,21 @@ function App() {
           tagStatus={tag}
         />
       </div>
+      <div className="w-full bg-[#151834] text-center text-gray-200 p-2">
+        <p className="mb-2 text-base">Join our Discord channel for engaging discussions on Competitive Programming (CP), Data Structures and Algorithms (DSA), and Placement opportunities!</p>
 
+        <a href="https://discord.gg/Pk3F2CW76p" target="_blank" className="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-orange-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-[#151834] group">
+          <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-[#2b2c3e] group-hover:h-full"></span>
+          <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
+            <svg className="w-5 h-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+          </span>
+          <span className="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
+            <svg className="w-5 h-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+          </span>
+          <span className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">ACodeDaily</span>
+        </a>
+
+      </div>
       <Footer />
     </div>
   );
--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
 import "./footer.css";
 import FooterCard from "./FooterCard";
 
+
 const Footer: React.FC = () => {
+
+  const [counter, setCounter] = useState(0);
+
+
+  const getData = async () => {
+    const response = await fetch("https://acodedaily.com/api/analysis");
+    if (response.status == 200) {
+      const data = await response.json();
+      console.log(typeof data.data[0].hits);
+      setCounter(data.data[0].hits);
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+
+
   return (
     <div className="footer">
       <div className="flex flex-wrap justify-evenly">
@@ -15,6 +36,12 @@ const Footer: React.FC = () => {
         <FooterCard name="Himanshu Raj" image="./assets/profile/default.png" linkedIn="#" />
         <FooterCard name="OM" image="./assets/profile/default.png" linkedIn="#" />
       </div>
+
+      <a href="https://discord.gg/Pk3F2CW76p" target="_blank" className="relative inline-block px-4 py-2 font-medium group">
+        <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+        <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+        <span className="relative text-black group-hover:text-white">Visit Count: {counter}</span>
+      </a>
 
       <span className="bottom">
         <img src="./assets/TextLogo.png" alt="ACD Ladders" />
