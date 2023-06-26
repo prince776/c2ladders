940e852a6c05a084e273133e9bce482e7d634395
thoughtlessnerd <abhay.csgo001@gmail.com>
2023-06-27 01:49:45 +0530

    added om's pic and a social bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -22,6 +22,7 @@ import {
 import Footer from "./components/Footer";
 
 import { toast } from "react-toastify";
+import SocialBar from "./components/SocialBar";
 
 function App() {
   const [user, setUser] = useState<string>("");
@@ -40,8 +41,7 @@ function App() {
 
   const [selected, setSelected] = useState(1200);
 
-  const [tag, setTag] = useState(false)
-
+  const [tag, setTag] = useState(false);
 
   const loadUser = async () => {
     try {
@@ -102,11 +102,6 @@ function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [userData]);
 
-  
-
-
-
-
   return (
     <div className="w-full bg-color">
       <Header heading="ACD Ladders!" />
@@ -134,7 +129,11 @@ function App() {
                   if (e.key === "Enter") loadUser();
                 }}
               />
-              <button type="submit" className="search-button" onClick={loadUser}>
+              <button
+                type="submit"
+                className="search-button"
+                onClick={loadUser}
+              >
                 <svg
                   width="20"
                   height="20"
@@ -154,7 +153,6 @@ function App() {
               </button>
             </div>
 
-
             <div className="ml-2 pt-1.5">
               <input
                 className="mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]"
@@ -163,9 +161,8 @@ function App() {
                 id="flexSwitchCheckDefault"
                 onChange={(e) => setTag(e.target.checked)}
               />
-              <label
-                className="inline pl-[0.15rem] hover:cursor-pointer text-gray-200 text-lg mr-1"
-              >{tag ? "Freq" : "Tags"}
+              <label className="inline pl-[0.15rem] hover:cursor-pointer text-gray-200 text-lg mr-1">
+                {tag ? "Freq" : "Tags"}
               </label>
             </div>
           </div>
@@ -181,19 +178,16 @@ function App() {
         />
       </div>
       <div className="w-full bg-[#151834] text-center text-gray-200 p-2">
-        <p className="mb-2 text-base">Join our Discord Server and be a part of an evergrowing community full of hustling & talented individuals discussing Competitive programming, DSA and Development! (This is free and would always be!)</p>
+        <p className="mt-5 mb-2 text-base">
+          <i>
+            Join our Discord Server and be a part of an evergrowing community
+            full of hustling & talented individuals discussing Competitive
+            programming, DSA and Development! (This is free and would always
+            be!)
+          </i>
+        </p>
 
-        <a href="https://discord.gg/Pk3F2CW76p" target="_blank" className="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-orange-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-[#151834] group">
-          <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-[#2b2c3e] group-hover:h-full"></span>
-          <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
-            <svg className="w-5 h-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
-          </span>
-          <span className="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
-            <svg className="w-5 h-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
-          </span>
-          <span className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">ACodeDaily</span>
-        </a>
-
+        <SocialBar />
       </div>
       <Footer />
     </div>
